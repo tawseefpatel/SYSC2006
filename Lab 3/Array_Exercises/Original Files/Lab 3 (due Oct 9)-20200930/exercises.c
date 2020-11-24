f59c85ccaa1f12ecae730e0b8d440161d5bc01ea
tawseefpatel <63173329+tawseefpatel@users.noreply.github.com>
2020-11-24 17:46:09 -0500

    Add files via upload

---
+++ Lab 3/Array_Exercises/Original Files/Lab 3 (due Oct 9)-20200930/exercises.c
@@ -0,0 +1,60 @@
+/* SYSC 2006 Lab 3. 
+
+ * Incomplete implementations of the functions that will be coded during the lab.
+ */
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#include "exercises.h"
+
+/* Exercise 1. */
+
+/* Return the average magnitude of the n doubles in x[]. 
+ * This function assumes that parameter n is >= 1.
+ */
+double avg_magnitude(double x[], double n)
+{
+    return -1.0;
+}
+
+/* Exercise 2. */
+
+/* Return the average power of the n doubles in x[].
+ * This function assumes that parameter n is >= 1.
+ */
+double avg_power(double x[], double n)
+{
+    return -1.0;
+}
+
+/* Exercise 3. */
+
+/* Return the largest of the n doubles in arr[]. 
+ * This function assumes that parameter n is >= 1.
+ */
+double max(double arr[], int n)
+{
+    return -1.0;
+}
+
+/* Exercise 4. */
+
+/* Return the smallest of the n doubles in arr[]. 
+ * This function assumes that parameter n is >= 1.
+ */
+double min(double arr[], int n)
+{
+    return -1.0;
+}
+
+/* Exercise 5. */
+
+/* Normalize the n doubles in x[]. 
+ * This function assumes that parameter n is >= 2, and that at least
+ * two of the values in x[] are different.
+ */
+void normalize(double x[], int n)
+{
+}
